cc7aab6996da5699fba4fe605e61ce28dc8eaadc
Rudik Avetisyan <rudikavetisyan6@gmail.com>
2025-11-12 13:38:55 +0400

    Remove show_reviews function from views.py

--- main_views/views.py
+++ main_views/views.py
@@ -8,8 +8,4 @@ def show_main(request):
 
 
 
-def show_reviews(request, book_slug):
-    book = get_object_or_404(Book, slug=book_slug)
-    reviews = Review.objects.filter(book=book)
-    return render(request, "reviews.html",{"book":book,
-                                           "reviews": reviews})
+
